38c71d9bbe7a48a6034528104628253a894bb4c0
alexanderpo <alexanderpometko@gmail.com>
2018-01-04 13:33:37 +0300

    Implement calendar event component

--- src/containers/Dashboard/Calendar.jsx
+++ src/containers/Dashboard/Calendar.jsx
@@ -2,15 +2,12 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { withRouter, Link } from 'react-router-dom';
 import _ from 'lodash';
 import moment from 'moment';
-import classNames from 'classnames';
-import { Paper, DatePicker, IconButton, Divider, CircularProgress } from 'material-ui';
-import MoreContentIcon from 'material-ui/svg-icons/navigation/more-horiz';
-import DeleteIcon from 'material-ui/svg-icons/action/delete';
+import { Paper, DatePicker, IconButton, CircularProgress } from 'material-ui';
 import ArrowIcon from 'material-ui/svg-icons/hardware/keyboard-arrow-down';
-import { getUpcomingEventsByDate } from '../../actions/user';
+import CalendarEvent from '../../components/Profile/Dashboard/CalendarEvent';
+import { getUpcomingEventsByDate, subscribeEventControl } from '../../actions/user';
 // import EventsSchedule from './EventsSchedule';
 
 const propTypes = {
@@ -19,6 +16,7 @@ const propTypes = {
   events: PropTypes.array,
   actions: PropTypes.shape({
     getUpcomingEventsByDate: PropTypes.func,
+    subscribeEventControl: PropTypes.func,
   }),
 };
 
@@ -33,9 +31,7 @@ class DashboardCalendar extends Component {
   }
 
   componentDidMount = () => {
-    const { userId, actions } = this.props;
-    const filteredDate = moment(this.state.date).format('YYYY-MM-DD');
-    actions.getUpcomingEventsByDate(userId, filteredDate);
+    this.updateEventsByDate();
   };
 
   getThisWeekend = () => {
@@ -43,6 +39,13 @@ class DashboardCalendar extends Component {
     const nextweek = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 7);
     return nextweek;
   }
+
+  updateEventsByDate = () => {
+    const { userId, actions } = this.props;
+    const filteredDate = moment(this.state.date).format('YYYY-MM-DD');
+    actions.getUpcomingEventsByDate(userId, filteredDate);
+  };
+
 
   handleChangeDatePicker = (event, date) => {
     const { userId, actions } = this.props;
@@ -55,42 +58,17 @@ class DashboardCalendar extends Component {
     return moment(new Date(this.state.date)).format('l');
   }
 
-  // TODO: open / show more info
-  // TODO: unsubscribe from event with dynamic update table
-  // TODO: added count of users whos gooing on event
-
   renderEvents = events => (
     !_.isEmpty(events) ? events.map(event => (
-      <div key={event.event_id} className="calendar-content__column">
-        <div className="calendar-main-content">
-          <div className="calendar__info">
-            { moment(event.event_datetime).format('H:MM') }
-          </div>
-          <div className="calendar__info calendar__title">
-            <Link to={`/events/${event.event_id}`} title={event.event_title} className="calendar-info__title">
-              {event.event_title}
-            </Link> at <Link to={`/playgrounds/${event.playground_id}`} title={event.playground_name} className="calendar-info__title">{event.playground_name}</Link>
-          </div>
-          <div className="calendar__actions">
-            <IconButton>
-              <MoreContentIcon className="dashboard-events-calendar-table__more-content-icon" />
-            </IconButton>
-          </div>
-        </div>
-        <div className="calendar-more-content">
-          <div>{event.playground_description}</div>
-          <div className="calendar-more-content__stats">
-            <span>Players - <span className="bolder__text">3</span></span>
-            <span>Organiser - <span className="bolder__text">{event.creator_name}</span></span>
-            <IconButton>
-              <DeleteIcon />
-            </IconButton>
-          </div>
-        </div>
-        <Divider />
-      </div>
+      <CalendarEvent
+        key={event.event_id}
+        userId={this.props.userId}
+        event={event}
+        unsubscribe={this.props.actions.subscribeEventControl}
+        update={this.updateEventsByDate}
+      />
     )) : (
-      <div>Don&apos;t have event&apos;s on this date</div>
+      <div className="calendar_empty__text">Don&apos;t have event&apos;s on this date</div>
     )
   );
 
@@ -142,8 +120,9 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
   actions: bindActionCreators({
     getUpcomingEventsByDate,
+    subscribeEventControl,
   }, dispatch),
 });
 
 DashboardCalendar.propTypes = propTypes;
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(DashboardCalendar));
+export default connect(mapStateToProps, mapDispatchToProps)(DashboardCalendar);
